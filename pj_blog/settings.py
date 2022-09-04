2de5d8792d3abb12e01fc6d2f7bbc61e58fcf5a5
kettle <teacues@gmail.com>
2022-09-04 22:55:49 +0900

    bugfix

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'js'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Tokyo'
 
