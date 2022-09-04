a16e599017d31d891cf353b940056526be9c6398
kettle <teacues@gmail.com>
2022-09-04 19:19:36 +0900

    汎用ビュー実装

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# ListViewとDetailViewを取り込み
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
+# ListViewは一覧を簡単に作るためのView
+class Index(ListView):
+    # 一覧するモデルを指定 -> `object_list`で取得可能
+    model = Post
 
-# Create your views here.
+# DetailViewは詳細を簡単に作るためのView
+class Detail(DetailView):
+    # 詳細表示するモデルを指定 -> `object`で取得可能
+    model = Post
+
+# CreateViewは新規作成画面を簡単に作るためのView
+class Create(CreateView):
+    model = Post
+    
+    # 編集対象にするフィールド
+    fields = ["title", "body", "category", "tags"]
+
+class Update(UpdateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+
+class Delete(DeleteView):
+    model = Post
+    
+    # 削除したあとに移動する先（トップページ）
+    success_url = "/"
