9b508427b23b642dcc94fbaf110cd4a29a541a1d
kettle <teacues@gmail.com>
2022-10-11 00:39:30 +0900

    listViewを作成

--- piechika/lifelog/views.py
+++ piechika/lifelog/views.py
@@ -2,5 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import sleepLog
 
-class Index(ListView):
+class sleeplogListView(ListView):
+    template_name = 'lifelog/sleeplogList.html'
+    context_object_name = 'sleeplogList'
     model = sleepLog
