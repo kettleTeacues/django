6e0dd6aeaf9f0bbc3dfa75863dd9519a3dec66d6
kettle <teacues@gmail.com>
2022-10-15 19:27:25 +0900

    viewを作成(list, detail)

--- piechika/lifelog/urls.py
+++ piechika/lifelog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sleeplogListView.as_view(), name='index')
+    path('', views.sleeplogListView.as_view(), name='index'),
+    path('sleeplog/detail/<int:pk>', views.sleeplogDetailView.as_view(), name='detail'),
 ]
--- piechika/lifelog/views.py
+++ piechika/lifelog/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import sleepLog
 
 class sleeplogListView(ListView):
     template_name = 'lifelog/sleeplogList.html'
     context_object_name = 'sleeplogList'
+    model = sleepLog
+
+class sleeplogDetailView(DetailView):
+    template_name = 'lifelog/sleeplogDetail.html'
+    context_object_name = 'sleeplogDetail'
     model = sleepLog
