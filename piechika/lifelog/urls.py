8b3cc91848d38a2f80e6f9e87c53051dddaddc05
kettle <teacues@gmail.com>
2022-10-15 20:06:50 +0900

    view(CURD)を作成

--- piechika/lifelog/urls.py
+++ piechika/lifelog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.sleeplogListView.as_view(), name='index'),
+    path('sleeplog/create/', views.sleeplogCreateView.as_view(), name='create'),
     path('sleeplog/detail/<int:pk>', views.sleeplogDetailView.as_view(), name='detail'),
+    path('sleeplog/update/<int:pk>', views.sleeplogUpdateView.as_view(), name='update'),
+    path('sleeplog/delete/<int:pk>', views.sleeplogDeleteView.as_view(), name='delete'),
 ]
--- piechika/lifelog/views.py
+++ piechika/lifelog/views.py
@@ -1,13 +1,45 @@
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from winreg import DeleteValue
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import sleepLog
 
 class sleeplogListView(ListView):
     template_name = 'lifelog/sleeplogList.html'
-    context_object_name = 'sleeplogList'
     model = sleepLog
+    context_object_name = 'sleeplogList'
 
 class sleeplogDetailView(DetailView):
     template_name = 'lifelog/sleeplogDetail.html'
-    context_object_name = 'sleeplogDetail'
     model = sleepLog
+    context_object_name = 'sleeplogDetail'
+
+class sleeplogCreateView(CreateView):
+    template_name = 'lifelog/sleeplogCreate.html'
+    model = sleepLog
+    fields = (
+        'userId',
+        'date',
+        'staDateTime',
+        'endDateTime',
+        'sleepingTime'
+    )
+    success_url = reverse_lazy('index')
+
+class sleeplogUpdateView(UpdateView):
+    template_name = 'lifelog/sleeplogUpdate.html'
+    model = sleepLog
+    context_object_name = 'sleeplogUpdate'
+    fields = (
+        'userId',
+        'date',
+        'staDateTime',
+        'endDateTime',
+        'sleepingTime'
+    )
+    success_url = reverse_lazy('index')
+
+class sleeplogDeleteView(DeleteView):
+    template_name = 'lifelog/sleeplogDelete.html'
+    model = sleepLog
+    context_object_name = 'sleeplogDelete'
+    success_url = reverse_lazy('index')
